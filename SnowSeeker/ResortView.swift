68a56bbff7233aa692502bcb104207b2cce9a76d
Henny2 <henrieke.baunack@web.de>
2024-08-18 13:53:18 -1000

    adding differing layout when space is limited

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ResortView: View {
     let resort: Resort
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass // tell us whether we have a regular or compact horizontal size class
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
     var body: some View {
         ScrollView{
             VStack(alignment: .leading, spacing: 0) {
@@ -17,8 +20,21 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack{
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing: 10) {
+                            ResortDetailsView(resort: resort)
+                        }
+                        VStack(spacing: 10) {
+                            SkiDetailsView(resort: resort)
+                        }
+                    } else {
+                        // both the following view use groups i.e. they are layout neutral
+                        // that's why they are put all in line based on the hstack
+                        // and in the if clause, they are stacked bc of the vstack
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
+                    
                 }
                 .padding(.vertical)
                 .background(.primary.opacity(0.1))
