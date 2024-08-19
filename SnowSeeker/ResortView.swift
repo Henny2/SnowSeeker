3d928da5402c7da71f6ea5d193078be7687d03ba
Henny2 <henrieke.baunack@web.de>
2024-08-18 14:30:12 -1000

    using icons and alerts for showing resort facilities

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -12,6 +12,9 @@ struct ResortView: View {
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass // tell us whether we have a regular or compact horizontal size class
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
     var body: some View {
         ScrollView{
             VStack(alignment: .leading, spacing: 0) {
@@ -44,14 +47,30 @@ struct ResortView: View {
                         .padding(.vertical)
                     Text("Facilities")
                         .font(.headline)
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.vertical)
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            Button {
+                                selectedFacility = facility
+                                showingFacility = true
+                            } label: {
+                                facility.icon
+                                    .font(.title)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
+        //presenting unwraps for us the optional
+        .alert(selectedFacility?.name ?? "more information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
+            // we don't care about the facility here, we are cool with just the default OK button
+        } message: { facility in
+            Text(facility.description)
+        }
     }
 }
 
